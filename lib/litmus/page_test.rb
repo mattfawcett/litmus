bb5d66c182301ae4cb85872dcc3117a5e2046821
Bobby Uhlenbrock <bobby@helloample.com>
2012-01-13 10:01:44 -0500

    Added sandbox flag for email and page tests

--- lib/litmus/page_test.rb
+++ lib/litmus/page_test.rb
@@ -4,13 +4,14 @@ module Litmus
       super.reject{|test| test["service"] != 'page'}
     end
 
-    def self.create(url, name = nil)
+    def self.create(url, name = nil, sandbox = false)
       builder = Builder::XmlMarkup.new
       builder.instruct! :xml, :version=>"1.0"
       builder.test_set do |test_set|
         test_set.use_defaults true
         test_set.url url
         test_set.name name if name
+        test_set.sandbox true if sandbox
       end
       post('/pages.xml', :body => builder.target!, :headers => {"Content-type" => "application/xml"})["test_set"]
     end
