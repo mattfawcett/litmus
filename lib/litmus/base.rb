be33706aad5e9a0785f99075f0c2b2e303e70412
Robert Graff <robert_graff@yahoo.com>
2014-11-04 06:23:51 -0800

    Use configure instead of initialize to set up base class.

--- lib/litmus/base.rb
+++ lib/litmus/base.rb
@@ -1,13 +1,16 @@
 module Litmus
   class Base
     include HTTParty
-    
+
     # debug_output $stderr
-    
     def initialize(company, username, password, ssl = false)
+      self.class.configure(company, username, password, ssl)
+    end
+
+    def self.configure(company, username, password, ssl = false)
       protocol = ssl ? 'https' : 'http'
-      self.class.base_uri "#{protocol}://#{company}.litmus.com"
-      self.class.basic_auth(username, password)
+      self.base_uri "#{protocol}://#{company}.litmus.com"
+      self.basic_auth(username, password)
     end
   end
 end
--- spec/litmus_spec.rb
+++ spec/litmus_spec.rb
@@ -2,13 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Litmus" do
 
-  describe ".initialize" do
+  describe ".configure" do
     it "should use the appropriate protocol for the given account" do
-      test = Litmus::Base.new('host', 'user', 'password')
-      test.class.base_uri.should == 'http://host.litmus.com'
-      test_ssl = Litmus::Base.new('host', 'user', 'password', true)
-      test_ssl.class.base_uri.should == 'https://host.litmus.com'
+      Litmus::Base.configure('host', 'user', 'password')
+      Litmus::Base.base_uri.should == 'http://host.litmus.com'
+      Litmus::Base.configure('host', 'user', 'password', true)
+      Litmus::Base.base_uri.should == 'https://host.litmus.com'
     end
   end
-  
+
 end
